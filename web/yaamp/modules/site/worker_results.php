81c1c9c1dbfcbf581b80a0c0bf5480985f14ebdd
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-07-19 01:34:41 +0200

    workers: show 'login' if set in account table

--- web/yaamp/modules/site/worker_results.php
+++ web/yaamp/modules/site/worker_results.php
@@ -12,19 +12,33 @@ echo "<th>Version</th>";
 echo "<th>Diff</th>";
 echo "<th>Hashrate</th>";
 echo "<th>Bad</th>";
+echo "<th>%</th>";
 echo "<th></th>";
-//echo "<th>Nonce1</th>";
 echo "</tr>";
 echo "</thead><tbody>";
 
 $workers = getdbolist('db_workers', "algo=:algo order by name", array(':algo'=>$algo));
+
+$total_rate = 0.0;
+foreach($workers as $worker)
+{
+	$total_rate += yaamp_worker_rate($worker->id);
+}
+
 foreach($workers as $worker)
 {
 	$user_rate = yaamp_worker_rate($worker->id);
+	$percent = 0.0;
+	if ($total_rate) $percent = (100.0 * $user_rate) / $total_rate;
 	$user_bad = yaamp_worker_rate_bad($worker->id);
-	$percent = ($user_rate+$user_bad)? round($user_bad*100/($user_rate+$user_bad), 3): 0;
+	$pct_bad = ($user_rate+$user_bad)? round($user_bad*100/($user_rate+$user_bad), 3): 0;
 	$user_rate = Itoa2($user_rate).'h/s';
-	$user_bad = Itoa2($user_bad).'h/s';
+
+	$name = $worker->worker;
+	if ($worker->userid) {
+		$user = getdbosql('db_accounts', 'id=:id', array(':id'=>$worker->userid));
+		$name = $user->login;
+	}
 
 	$dns = !empty($worker->dns)? $worker->dns: $worker->ip;
 	if(strlen($worker->dns) > 40)
@@ -37,14 +51,19 @@ foreach($workers as $worker)
 	echo "<td>$worker->version</td>";
 	echo "<td>$worker->difficulty</td>";
 	echo "<td>$user_rate</td>";
-	echo "<td>$user_bad</td>";
 
-	if($percent > 50)
-		echo "<td align=right><b>{$percent}%</b></td>";
-	else
-		echo "<td align=right>{$percent}%</td>";
+	echo "<td>". ($user_bad ? Itoa2($user_bad).'h/s' : '-');
+	if ($user_bad) {
+		if ($pct_bad > 50)
+			echo "<b> {$pct_bad}%</b>";
+		else
+			echo " {$pct_bad}%";
+	}
+	echo "</td>";
 
-//	echo "<td>$worker->nonce1</td>";
+	echo '<td>'.number_format($percent,1,'.','').'%</td>';
+
+	echo "<td>$name</td>";
 	echo "</tr>";
 }
 
