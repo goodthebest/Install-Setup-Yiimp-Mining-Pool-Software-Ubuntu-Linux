3d2581b4f4d6ad532b55b7274cefc12da6e3c966
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-07-20 20:54:57 +0200

    shell: add a maximum execution time on screen tasks

--- web/blocks.sh
+++ web/blocks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias php5='php -d max_execution_time=60'
+
 cd /var/web
 while true; do
         php5 run.php cronjob/runblocks
--- web/main.sh
+++ web/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+alias php5='php -d max_execution_time=120'
+
 cd /var/web
 while true; do
         php5 run.php cronjob/run
