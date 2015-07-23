9476a525bd4c8834e79a48f8632fe217a3a475b6
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-07-23 14:31:00 +0200

    history_results: show also coins without recent blocks

--- web/yaamp/modules/site/results/history_results.php
+++ web/yaamp/modules/site/results/history_results.php
@@ -30,25 +30,38 @@ $total2 = 0;
 $total3 = 0;
 $total4 = 0;
 
+$main_ids = array();
+
 $algo = user()->getState('yaamp-algo');
-$list = dbolist("SELECT coin_id FROM blocks where category!='orphan' and time>$t4 and coin_id in (select id from coins where algo=:algo) group by coin_id order by id desc", array(':algo'=>$algo));
+$list = dbolist("SELECT coin_id FROM blocks WHERE coin_id IN (select id from coins where algo=:algo)
+	AND time>$t4 AND category!='orphan' GROUP BY coin_id ORDER BY coin_id DESC",
+	array(':algo'=>$algo)
+);
 
 foreach($list as $item)
 {
 	$coin = getdbo('db_coins', $item['coin_id']);
+
+	$id = $coin->id;
+	$main_ids[$id] = $coin->symbol;
+
 	if($coin->symbol == 'BTC') continue;
 
-	$res1 = controller()->memcache->get_database_row("history_item1-$coin->id-$algo",
-		"select count(*) as a, sum(amount*price) as b from blocks where category!='orphan' and time>$t1 and coin_id=$coin->id and algo=:algo", array(':algo'=>$algo));
+	$res1 = controller()->memcache->get_database_row("history_item1-$id-$algo",
+		"SELECT COUNT(*) as a, SUM(amount*price) as b FROM blocks WHERE coin_id=$id AND category!='orphan' AND time>$t1 AND algo=:algo", 
+		array(':algo'=>$algo));
 
-	$res2 = controller()->memcache->get_database_row("history_item2-$coin->id-$algo",
-		"select count(*) as a, sum(amount*price) as b from blocks where category!='orphan' and time>$t2 and coin_id=$coin->id and algo=:algo", array(':algo'=>$algo));
+	$res2 = controller()->memcache->get_database_row("history_item2-$id-$algo",
+		"SELECT COUNT(*) as a, SUM(amount*price) as b FROM blocks WHERE coin_id=$id AND category!='orphan' AND time>$t2 AND algo=:algo", 
+		array(':algo'=>$algo));
 
-	$res3 = controller()->memcache->get_database_row("history_item3-$coin->id-$algo",
-		"select count(*) as a, sum(amount*price) as b from blocks where category!='orphan' and time>$t3 and coin_id=$coin->id and algo=:algo", array(':algo'=>$algo));
+	$res3 = controller()->memcache->get_database_row("history_item3-$id-$algo",
+		"SELECT COUNT(*) as a, SUM(amount*price) as b FROM blocks WHERE coin_id=$id AND category!='orphan' AND time>$t3 AND algo=:algo", 
+		array(':algo'=>$algo));
 
-	$res4 = controller()->memcache->get_database_row("history_item4-$coin->id-$algo",
-		"select count(*) as a, sum(amount*price) as b from blocks where category!='orphan' and time>$t4 and coin_id=$coin->id and algo=:algo", array(':algo'=>$algo));
+	$res4 = controller()->memcache->get_database_row("history_item4-$id-$algo",
+		"SELECT COUNT(*) as a, SUM(amount*price) as b FROM blocks WHERE coin_id=$id AND category!='orphan' AND time>$t4 AND algo=:algo", 
+		array(':algo'=>$algo));
 
 	$total1 += $res1['b'];
 	$total2 += $res2['b'];
@@ -57,32 +70,48 @@ foreach($list as $item)
 
 	$name = substr($coin->name, 0, 12);
 
-	echo "<tr class='ssrow'>";
+	echo '<tr class="ssrow">';
 
-	echo "<td width=18><img width=16 src='$coin->image'></td>";
-	echo "<td><b><a href='/site/block?id=$coin->id'>$name</a></b></td>";
+	echo '<td width=18><img width=16 src="'.$coin->image.'"></td>';
+	echo '<td><b><a href="/site/block?id='.$id.'">'.$name.'</a></b></td>';
 
-	echo "<td align=right style='font-size: .9em;'>{$res1['a']}</td>";
-	echo "<td align=right style='font-size: .9em;'>{$res2['a']}</td>";
-	echo "<td align=right style='font-size: .9em;'>{$res3['a']}</td>";
-	echo "<td align=right style='font-size: .9em;'>{$res4['a']}</td>";
+	echo '<td align="right" style="font-size: .9em;">'.$res1['a'].'</td>';
+	echo '<td align="right" style="font-size: .9em;">'.$res2['a'].'</td>';
+	echo '<td align="right" style="font-size: .9em;">'.$res3['a'].'</td>';
+	echo '<td align="right" style="font-size: .9em;">'.$res4['a'].'</td>';
 
-	echo "</tr>";
+	echo '</tr>';
+}
+
+$others = dbolist("SELECT id, image, symbol, name FROM coins
+	WHERE algo=:algo AND installed=1 AND enable=1 AND auto_ready=1 AND visible=1 ORDER BY symbol",
+	array(':algo'=>$algo)
+);
+
+foreach($others as $item)
+{
+	if (array_key_exists($item['id'], $main_ids))
+		continue;
+	echo '<tr class="ssrow">';
+	echo '<td width="18px"><img width="16px" src="'.$item['image'].'"></td>';
+	echo '<td><b><a href="/site/block?id='.$item['id'].'">'.$item['name'].'</a></b></td>';
+	echo '<td colspan="4"></td>';
+	echo '</tr>';
 }
 
 ///////////////////////////////////////////////////////////////////////
 
 $hashrate1 = controller()->memcache->get_database_scalar("history_hashrate1-$algo",
-	"select avg(hashrate) from hashrate where time>$t1 and algo=:algo", array(':algo'=>$algo));
+	"SELECT AVG(hashrate) FROM hashrate WHERE time>$t1 AND algo=:algo", array(':algo'=>$algo));
 
 $hashrate2 = controller()->memcache->get_database_scalar("history_hashrate2-$algo",
-	"select avg(hashrate) from hashrate where time>$t2 and algo=:algo", array(':algo'=>$algo));
+	"SELECT AVG(hashrate) FROM hashrate WHERE time>$t2 AND algo=:algo", array(':algo'=>$algo));
 
 $hashrate3 = controller()->memcache->get_database_scalar("history_hashrate3-$algo",
-	"select avg(hashrate) from hashrate where time>$t3 and algo=:algo", array(':algo'=>$algo));
+	"SELECT AVG(hashrate) FROM hashrate WHERE time>$t3 AND algo=:algo", array(':algo'=>$algo));
 
 $hashrate4 = controller()->memcache->get_database_scalar("history_hashrate4-$algo",
-	"select avg(hashrate) from hashstats where time>$t4 and algo=:algo", array(':algo'=>$algo));
+	"SELECT AVG(hashrate) FROM hashstats WHERE time>$t4 AND algo=:algo", array(':algo'=>$algo));
 
 $hashrate1 = max($hashrate1 , 1);
 $hashrate2 = max($hashrate2 , 1);
@@ -104,50 +133,50 @@ $total2 = bitcoinvaluetoa($total2);
 $total3 = bitcoinvaluetoa($total3);
 $total4 = bitcoinvaluetoa($total4);
 
-echo "<tr class='ssrow' style='border-top: 2px solid #eee;'>";
-echo "<td width=18><img width=16 src='/images/btc.png'></td>";
-echo "<td><b>BTC Value</b></td>";
+echo '<tr class="ssrow" style="border-top: 2px solid #eee;">';
+echo '<td width="18px"><img width="16px" src="/images/btc.png"></td>';
+echo '<td><b>BTC Value</b></td>';
 
-echo "<td align=right style='font-size: .9em;'>$total1</td>";
-echo "<td align=right style='font-size: .9em;'>$total2</td>";
-echo "<td align=right style='font-size: .9em;'>$total3</td>";
-echo "<td align=right style='font-size: .9em;'>$total4</td>";
-
-echo "</tr>";
-
-///////////////////////////////////////////////////////////////////////
-
-echo "<tr class='ssrow' style='border-top: 2px solid #eee;'>";
-echo "<td width=18></td>";
-echo "<td><b>Avg Hashrate</b></td>";
-
-echo "<td align=right style='font-size: .9em;'>{$hashrate1}h/s</td>";
-echo "<td align=right style='font-size: .9em;'>{$hashrate2}h/s</td>";
-echo "<td align=right style='font-size: .9em;'>{$hashrate3}h/s</td>";
-echo "<td align=right style='font-size: .9em;'>{$hashrate4}h/s</td>";
+echo '<td align="right" style="font-size: .9em;">'.$total1.'</td>';
+echo '<td align="right" style="font-size: .9em;">'.$total2.'</td>';
+echo '<td align="right" style="font-size: .9em;">'.$total3.'</td>';
+echo '<td align="right" style="font-size: .9em;">'.$total4.'</td>';
 
 echo "</tr>";
 
 ///////////////////////////////////////////////////////////////////////
 
-echo "<tr class='ssrow' style='border-top: 2px solid #eee;'>";
-echo "<td width=18></td>";
-echo "<td><b>mBTC/Mh/d</b></td>";
+echo '<tr class="ssrow" style="border-top: 2px solid #eee;">';
+echo '<td width="18px"></td>';
+echo '<td><b>Avg Hashrate</b></td>';
 
-echo "<td align=right style='font-size: .9em;'>$btcmhday1</td>";
-echo "<td align=right style='font-size: .9em;'>$btcmhday2</td>";
-echo "<td align=right style='font-size: .9em;'>$btcmhday3</td>";
-echo "<td align=right style='font-size: .9em;'>$btcmhday4</td>";
+echo '<td align="right" style="font-size: .9em;">'.$hashrate1.'h/s</td>';
+echo '<td align="right" style="font-size: .9em;">'.$hashrate2.'h/s</td>';
+echo '<td align="right" style="font-size: .9em;">'.$hashrate3.'h/s</td>';
+echo '<td align="right" style="font-size: .9em;">'.$hashrate4.'h/s</td>';
 
-echo "</tr>";
+echo '</tr>';
 
-echo "</table>";
+///////////////////////////////////////////////////////////////////////
+
+echo '<tr class="ssrow" style="border-top: 2px solid #eee;">';
+echo '<td width="18px"></td>';
+echo '<td><b>mBTC/Mh/d</b></td>';
+
+echo '<td align="right" style="font-size: .9em;">'.$btcmhday1.'</td>';
+echo '<td align="right" style="font-size: .9em;">'.$btcmhday2.'</td>';
+echo '<td align="right" style="font-size: .9em;">'.$btcmhday3.'</td>';
+echo '<td align="right" style="font-size: .9em;">'.$btcmhday4.'</td>';
+
+echo '</tr>';
+
+echo '</table>';
 
 
-echo "</div>";
+echo '</div>';
 
-echo "<br>";
-echo "</div></div><br>";
+echo '<br>';
+echo '</div></div><br>';
 
 
 
