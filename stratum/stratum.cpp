1ff8ce1afa5f0079c510f491ae0c1576aae9228c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-08-18 11:16:32 +0200

    lyra2v2: change diff to 256

--- stratum/stratum.cpp
+++ stratum/stratum.cpp
@@ -93,7 +93,7 @@ YAAMP_ALGO g_algos[] =
 	{"x15", x15_hash, 1, 0, 0},
 
 	{"lyra2", lyra2re_hash, 0x80, 0, 0},
-	{"lyra2v2", lyra2v2_hash, 0x80, 0, 0},
+	{"lyra2v2", lyra2v2_hash, 0x100, 0, 0},
 
 	{"blake", blake_hash, 1, 0, 0},
 	{"fresh", fresh_hash, 0x100, 0, 0},
