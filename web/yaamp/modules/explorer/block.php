b4b235f249e71957ec8a88f2a95ae61bb7837f0d
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-03-17 21:24:19 +0100

    explorer: show raw transactions + decred stake

--- web/yaamp/modules/explorer/block.php
+++ web/yaamp/modules/explorer/block.php
@@ -1,14 +1,20 @@
 <?php
 
+if (!$coin) return;
+
 $this->pageTitle = $coin->name." bloc explorer";
 
-if ($coin) echo <<<ENDJS
-	<script>
-	$(function() {
-		$('#favicon').remove();
-		$('head').append('<link href="{$coin->image}" id="favicon" rel="shortcut icon">');
-	});
-	</script>
+echo <<<ENDJS
+<script type="text/javascript">
+function toggleRaw(el) {
+	$(el).parents('tr').next('tr.raw').toggle();
+}
+$(function() {
+	$('#favicon').remove();
+	$('head').append('<link href="{$coin->image}" id="favicon" rel="shortcut icon">');
+	$('span.txid').bind('click', function(el) { toggleRaw(el.target); });
+});
+</script>
 ENDJS;
 
 function simplifyscript($script)
@@ -68,16 +74,23 @@ echo "</table><br>";
 
 ////////////////////////////////////////////////////////////////////////////////
 
-echo "<table class='dataGrid2'>";
+echo <<<end
+<style type="text/css">
+span.txid { font-family: monospace; cursor: pointer; }
+tr.raw td { overflow-x: scroll; max-width: 1880px;}
+pre.json { font-size: 10px; }
+</style>
 
-echo "<thead>";
-echo "<tr>";
-echo "<th>Transaction Hash</th>";
-echo "<th>Value</th>";
-echo "<th>From (amount)</th>";
-echo "<th>To (amount)</th>";
-echo "</tr>";
-echo "</thead>";
+<table class="dataGrid2">
+<thead>
+<tr>
+<th>Transaction Hash</th>
+<th>Value</th>
+<th>From</th>
+<th>To (amount)</th>
+</tr>
+</thead>
+end;
 
 foreach($block['tx'] as $txhash)
 {
@@ -90,15 +103,13 @@ foreach($block['tx'] as $txhash)
 
 	echo "<tr class='ssrow'>";
 
-	echo "<td><span style='font-family: monospace;'><a href='/explorer?id=$coin->id&txid={$tx['txid']}'>{$tx['txid']}</a></span></td>";
+	echo '<td><span class="txid">'.$tx['txid'].'</span></td>';
 	echo "<td>$valuetx</td>";
 
 	echo "<td>";
-	foreach($tx['vin'] as $vin)
-	{
+	foreach($tx['vin'] as $vin) {
 		if(isset($vin['coinbase']))
 			echo "Generation";
-
 	}
 	echo "</td>";
 
@@ -106,9 +117,10 @@ foreach($block['tx'] as $txhash)
 	foreach($tx['vout'] as $vout)
 	{
 		$value = $vout['value'];
+		if ($value == 0) continue;
 
 		if(isset($vout['scriptPubKey']['addresses'][0]))
-			echo "<span style='font-family: monospace;'>{$vout['scriptPubKey']['addresses'][0]}</span> ($value)";
+			echo '<span style="font-family: monospace;">'.$vout['scriptPubKey']['addresses'][0]."</span> ($value)";
 		else
 			echo "($value)";
 
@@ -116,16 +128,77 @@ foreach($block['tx'] as $txhash)
 	}
 	echo "</td>";
 
+//	if (user()->getState('yaamp_admin')) {
+		echo '</tr><tr class="raw" style="display:none;"><td colspan="4"><pre class="json">';
+		unset($tx['hex']);
+		echo json_encode($tx, 128);
+		echo '</pre></td>';
+//	}
+
 	echo "</tr>";
 }
 
-echo '</table><pre class="json">';
+if ($coin->symbol == 'DCR') {
 
-if (user()->getState('yaamp_admin'))
-	echo json_encode($block, 128);
+	echo '<tr><th colspan="4">';
+	echo '<b>Stake</b>';
+	echo '</th></tr>';
+
+	foreach($block['stx'] as $txhash)
+	{
+		$stx = $remote->getrawtransaction($txhash, 1);
+		if(!$stx) continue;
+
+		$valuetx = 0;
+		foreach($stx['vout'] as $vout)
+			$valuetx += $vout['value'];
+
+		echo '<tr class="ssrow">';
+		echo '<td><span class="txid">'.$stx['txid'].'</span></td>';
+		echo "<td>$valuetx</td>";
+
+		echo "<td>";
+		foreach($stx['vin'] as $vin) {
+			if(arraySafeVal($vin,'blockheight') > 0) {
+				echo '<a href="/explorer?id='.$coin->id.'&height='.$vin['blockheight'].'">'.$vin['blockheight'].'</a>';
+				echo '<br/>';
+			}
+		}
+		echo "</td>";
+
+		echo "<td>";
+		foreach($stx['vout'] as $vout)
+		{
+			$value = $vout['value'];
+			if ($value == 0) continue;
+
+			if(isset($vout['scriptPubKey']['addresses'][0]))
+				echo '<span style="font-family: monospace;">'.$vout['scriptPubKey']['addresses'][0]."</span> ($value)";
+			else
+				echo "($value)";
+
+			echo '<br/>';
+		}
+		echo "</td>";
+
+//		if (user()->getState('yaamp_admin')) {
+			echo '</tr><tr class="raw" style="display:none;"><td colspan="4"><pre class="json">';
+			unset($stx['hex']);
+			echo json_encode($stx, 128);
+			echo '</pre></td>';
+//		}
+
+		echo '</tr>';
+	}
+}
+
+echo '</table>';
+
+if (user()->getState('yaamp_admin')) {
+	echo '<pre class="json">'.json_encode($block, 128).'</pre>';
+}
 
 echo <<<end
-</pre>
 <form action="/explorer" method="get" style="padding: 10px;">
 <input type="hidden" name="id" value="$coin->id">
 <input type="text" name="height" class="main-text-input" placeholder="block height" style="width: 80px;">
