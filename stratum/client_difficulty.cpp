a6136712d29e1813ba784c98858130da31944381
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-05-19 22:53:27 +0200

    stratum: prevent diff/algo setting conflict

--- stratum/client_difficulty.cpp
+++ stratum/client_difficulty.cpp
@@ -84,7 +84,8 @@ int client_send_difficulty(YAAMP_CLIENT *client, double difficulty)
 void client_initialize_difficulty(YAAMP_CLIENT *client)
 {
 	char *p = strstr(client->password, "d=");
-	if(!p) return;
+	char *p2 = strstr(client->password, "decred=");
+	if(!p || p2) return;
 
 	double diff = client_normalize_difficulty(atof(p+2));
 	uint64_t user_target = diff_to_target(diff);
