95fe2449842d93b8b761daabe32f162e5b736464
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-09-11 19:56:21 +0200

    basic cryptonote wallet rpc class/converter
    allow to read balance and do queries via the console
    
    Note: The db is not ready to handle both wallet and daemons ip/ports
          I assume the wallet is on local host and the daemon on another machine(ip).
          For now, they should use the same port (on 2 different ips so).

--- web/yaamp/core/rpc/rpc.php
+++ web/yaamp/core/rpc/rpc.php
@@ -3,4 +3,5 @@
 require_once('easybitcoin.php');
 require_once('json-rpc.php');
 require_once('ethereum.php');
+require_once('xmr-rpc.php');
 require_once('wallet-rpc.php');
--- web/yaamp/core/rpc/wallet-rpc.php
+++ web/yaamp/core/rpc/wallet-rpc.php
@@ -6,10 +6,12 @@ class WalletRPC {
 
 	public $type = 'Bitcoin';
 	protected $rpc;
+	protected $rpc_wallet;
 
 	// cache
 	protected $account;
 	protected $accounts;
+	protected $coin;
 	protected $info;
 	protected $height = 0;
 
@@ -29,6 +31,13 @@ class WalletRPC {
 				$this->type = 'Ethereum';
 				$this->account = empty($coin->account) ? $coin->master_wallet : $coin->account;
 				$this->rpc = new Ethereum($coin->rpchost, $coin->rpcport);
+				break;
+			case 'XMR':
+				$this->type = 'CryptoNote';
+				$this->rpc = new CryptoRPC($coin->rpchost, $coin->rpcport, $coin->rpcuser, $coin->rpcpasswd);
+				// for now, assume the wallet in on localhost, and daemon set in the db
+				$this->rpc_wallet = new CryptoRPC("127.0.0.1", $coin->rpcport, $coin->rpcuser, $coin->rpcpasswd);
+				$this->coin = $coin;
 				break;
 			default:
 				$this->type = 'Bitcoin';
@@ -132,6 +141,170 @@ class WalletRPC {
 				$this->error = $this->rpc->error;
 				return $res;
 			}
+		}
+
+		// XMR & BBR
+		else if ($this->type == 'CryptoNote')
+		{
+			// convert some rpc methods used by yiimp (and handle wallet rpc, very limited)
+			switch ($method) {
+			case "getinfo":
+				$res = $this->rpc->getinfo();
+				$res["blocks"] = arraySafeVal($res,"height");
+				$res["connections"] = arraySafeVal($res,"white_peerlist_size");
+				$balances = $this->rpc_wallet->getbalance();
+				$res["balance"] = arraySafeVal($balances,"unlocked_balance") / 1e12;
+				$res["pending"] = arraySafeVal($balances,"balance",0) / 1e12 - $res["balance"];
+				$ver = arraySafeVal($res,"mi");
+				$res["version"] = (int) sprintf("%02d%02d%02d%02d", // BBR
+					arraySafeVal($ver,"ver_major"), arraySafeVal($ver,"ver_minor"), arraySafeVal($ver,"ver_revision"), arraySafeVal($ver,"build_no"));
+				$this->error = $this->rpc_wallet->error.$this->rpc->error;
+				unset($res["mi"]);
+				break;
+			case "getmininginfo":
+				$res = $this->rpc->getinfo();
+				$res["networkhps"] = arraySafeVal($res,"current_network_hashrate_50");
+				unset($res["current_network_hashrate_50"]);
+				unset($res["current_network_hashrate_350"]);
+				unset($res["mi"]);
+				unset($res["grey_peerlist_size"]);
+				unset($res["white_peerlist_size"]);
+				unset($res["incoming_connections_count"]);
+				unset($res["outgoing_connections_count"]);
+				unset($res["max_net_seen_height"]);
+				unset($res["synchronization_start_height"]);
+				unset($res["transactions_cnt_per_day"]);
+				unset($res["transactions_volume_per_day"]);
+				unset($res["mi"]);
+				$data = $this->rpc->getlastblockheader();
+				$header = arraySafeVal($data,"block_header");
+				$res["reward"] = (double) arraySafeVal($header,"reward") / 1e12;
+				$this->error = $this->rpc->error;
+				break;
+			case "getnetworkinfo":
+				$res = $this->rpc->getinfo();
+				$res["connections"] = arraySafeVal($res,"white_peerlist_size");
+				$res["networkhps"] = arraySafeVal($res,"current_network_hashrate_50");
+				unset($res["current_network_hashrate_50"]);
+				unset($res["current_network_hashrate_350"]);
+				unset($res["mi"]);
+				$this->error = $this->rpc->error;
+				break;
+			case 'getaccountaddress':
+				$res = $this->rpc_wallet->getaddress();
+				$res = objSafeVal($res, "address");
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'getblocktemplate':
+				$gbt_params = array(
+					"wallet_address" => $this->coin->master_wallet,
+					"reserve_size"   => 8, // extra data
+					"alias_details"  => null,
+				);
+				$res = $this->rpc->getblocktemplate($gbt_params);
+				$data = $this->rpc->getlastblockheader();
+				$header = arraySafeVal($data,"block_header");
+				$res["coinbase"] = (double) arraySafeVal($header,"reward") / 1e4;
+				$res["reward"] = (double) arraySafeVal($header,"reward") / 1e12;
+				$this->error = $this->rpc->error;
+				break;
+			case "getbalance":
+				$res = $this->rpc_wallet->getbalance();
+				$this->error = $this->rpc_wallet->error;
+				$res = arraySafeVal($res,"unlocked_balance") / 1e12;
+				break;
+			case "getbalances":
+				$res = $this->rpc_wallet->getbalance();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'listtransactions':
+				$res = $this->rpc_wallet->get_bulk_payments();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'getaddress':
+				$res = $this->rpc_wallet->getaddress();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'get_bulk_payments':
+				$res = $this->rpc_wallet->get_bulk_payments();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'get_payments':
+				$named_params = array(
+					"payment_id"=>arraySafeVal($params, 0)
+				);
+				$res = $this->rpc_wallet->get_payments($named_params);
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'get_transfers': // deprecated ?
+				$res = $this->rpc_wallet->get_transfers();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'incoming_transfers': // deprecated ?
+				$res = $this->rpc_wallet->incoming_transfers();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'sendtoaddress':
+				// 3rd param is "payment id"
+				$destination = array(
+					"address"=>arraySafeVal($params, 0),
+					"amount"=> (double) arraySafeVal($params, 1) * 1e12,
+				);
+				$named_params = array(
+					"mixin"=>0,
+					"destinations" => array((object)$destination),
+					"payment_id" => arraySafeVal($params, 2),
+				);
+				$res = $this->rpc_wallet->transfer($named_params);
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'sendmany':
+				$destinations = array();
+				foreach ($params as $dest) {
+					foreach ($dest as $addr => $amount) {
+						$data = array("amount" => (double) $amount * 1e12, "address"=>$addr);
+						$destinations[] = (object) $data;
+					}
+				}
+				$named_params = array(
+					"mixin"=>arraySafeVal($params, 0, 0),
+					"destinations"=>$destinations,
+				);
+				$res = $this->rpc_wallet->transfer($named_params);
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'transfer':
+			case 'transfer_original':
+				$destination = array(
+					"address"=> arraySafeVal($params, 1),
+					"amount"=> (double) arraySafeVal($params, 2, 0) * 1e12,
+					// also: "fee" "unlock_time"
+				);
+				$destinations = array();
+				$destinations[] = (object)$destination;
+				$named_params = array(
+					"mixin" => arraySafeVal($params, 0, 0),
+					"destinations" => $destinations,
+					"payment_id" => arraySafeVal($params, 3),
+				);
+				$res = $this->rpc_wallet->transfer($named_params);
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'reset':
+				$res = $this->rpc_wallet->reset();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			case 'store':
+				$res = $this->rpc_wallet->store();
+				$this->error = $this->rpc_wallet->error;
+				break;
+			default:
+				// default to daemon
+				$res = $this->rpc->__call($method,$params);
+				$this->error = $this->rpc->error;
+			}
+
+			return $res;
 		}
 
 		// Bitcoin RPC
