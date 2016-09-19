413ff39b175cffd5195a9e3aacd3b306ff781281
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-09-20 00:46:27 +0200

    api: improve the doc json alignment

--- web/yaamp/modules/site/api.php
+++ web/yaamp/modules/site/api.php
@@ -36,14 +36,14 @@ result:
 	"paid": 0.00000000,
 	"total": 0.00050362,
 	"miners":[{
-		"version":"ccminer/1.8.2",
-		"password":"d=96",
-		"ID":"",
-		"algo":"decred",
-		"difficulty":96,
-		"subscribe":1,
-		"accepted":82463372.083,
-		"rejected":0
+		"version": "ccminer\/1.8.2",
+		"password": "d=96",
+		"ID": "",
+		"algo": "decred",
+		"difficulty": 96,
+		"subscribe": 1,
+		"accepted": 82463372.083,
+		"rejected": 0
 	}]
 }
 </pre>
@@ -59,12 +59,12 @@ result:
 <pre class="main-left-box" style='padding: 3px; font-size: .8em; background-color: #ffffee; font-family: monospace;'>
 {
 	"x11": {
-		"name": "blake",
-		"port": 5733,
+		"name": "x11",
+		"port": 3533,
 		"coins": 10,
 		"fees": 1,
 		"hashrate": 269473938,
-		"workers": 1,
+		"workers": 5,
 		"estimate_current": "0.00053653",
 		"estimate_last24h": "0.00036408",
 		"actual_last24h": "0.00035620",
