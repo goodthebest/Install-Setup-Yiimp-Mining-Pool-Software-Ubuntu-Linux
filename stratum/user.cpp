9121aa8f232768fdb7682ba2e72e368cf25892c4
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2017-01-06 18:10:01 +0100

    stratum: prevent account coin symbol change with a balance

--- stratum/user.cpp
+++ stratum/user.cpp
@@ -90,7 +90,7 @@ void db_add_user(YAAMP_DB *db, YAAMP_CLIENT *client)
 	}
 
 	else
-		db_query(db, "UPDATE accounts SET coinsymbol='%s', donation=%d WHERE id=%d", symbol, gift, client->userid);
+		db_query(db, "UPDATE accounts SET coinsymbol='%s', donation=%d WHERE id=%d AND balance=0", symbol, gift, client->userid);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
