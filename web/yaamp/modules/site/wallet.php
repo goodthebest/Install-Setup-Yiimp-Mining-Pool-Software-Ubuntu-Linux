e8f1d80b1d0f8246347753630f07091ac700adf8
DumaxFr <35855637+DumaxFr@users.noreply.github.com>
2018-06-30 05:17:30 +0200

    User wallet: disable deletion of current wallet (#280)

--- web/yaamp/modules/site/wallet.php
+++ web/yaamp/modules/site/wallet.php
@@ -141,9 +141,10 @@ foreach($recents as $addr)
 		$balance = $balance>0? "$balance BTC": '';
 
 	echo '<td align="right">'.$balance.'</td>';
-	echo '<td style="width: 16px; max-width: 16px;">'.
-		'<img src="/images/base/delete.png" onclick="javascript:drop_cookie(this);" style="cursor:pointer;"/>'.
-	'</td>';
+	
+	$delicon = $address == $addr ? '' : '<img src="/images/base/delete.png" onclick="javascript:drop_cookie(this);" style="cursor:pointer;"/>';
+	echo '<td style="width: 16px; max-width: 16px;">'.$delicon.'</td>';
+	
 	echo '</tr>';
 }
 
