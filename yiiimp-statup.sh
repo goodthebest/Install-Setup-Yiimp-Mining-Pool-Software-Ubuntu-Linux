27f7483102c758cea77eee2e6716d85fedfebd8d
Linux server management, OS softwares and applications, blockchain Mining Pools, Daemons <goodthebest123@yahoo.com>
2022-12-20 19:17:12 +0500

    Delete yiiimp-statup.sh

--- yiiimp-statup.sh
+++
@@ -1,7 +0,0 @@
-sudo killall screen
-sudo screen -dmS main $WEB_DIR/main.sh
-sudo screen -dmS loop2 $WEB_DIR/loop2.sh
-sudo screen -dmS blocks $WEB_DIR/blocks.sh
-# Stratum instances
-
-#sudo screen -dmS [algoname] $STRATUM_DIR/run.sh [algoname]
