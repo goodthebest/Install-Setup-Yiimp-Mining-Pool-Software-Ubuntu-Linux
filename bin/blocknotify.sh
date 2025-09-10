9a3566fba3a2fecdd9591c05a4e26fc4a7c364a1
Linux server management, OS softwares and applications, blockchain Mining Pools, Daemons <goodthebest123@yahoo.com>
2025-09-11 00:55:50 +0500

    Delete bin directory

--- bin/blocknotify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# callback script... replaced by blocknotify binary
-#
-# blocknotify="blocknotify.sh --host yaamp --port $stratum_port --id $coin->id --block %s --password tu8tu5"
-
